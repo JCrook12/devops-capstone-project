73940a68925f0dc77b45c0f6b7045f3878f3f59a
Justin Crook <justin.j.crook.civ@us.navy.mil>
2024-10-30 15:19:20 -0400

    Updated test_routes.py and routes.py to include support for listing all accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -141,3 +141,12 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/{0}",content_type="application/json")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    # Test Case for Listing All Accounts
+    def test_get_account_list(self):
+        """It should get a list of accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data),5)
